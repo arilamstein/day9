ea9a73622798eceecade672fa4caf31fa3b63c7d
Ari Lamstein <arilamstein@gmail.com>
2023-12-28 09:34:11 -0800

    initial commit

---
+++ day9.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.header("Line graph of random numbers")
+
+df = pd.DataFrame({'y': np.random.rand(100)})
+
+st.write("Here's the dataframe I'll be charting", df.head())
+
+st.write("And here's the chart!")
+
+st.line_chart(df, y="y")
