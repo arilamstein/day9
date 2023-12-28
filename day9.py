4026b7300dfbc8a79d845ee5e1105fd0ee46b3c1
Ari Lamstein <arilamstein@gmail.com>
2023-12-28 10:00:52 -0800

    add color selection (day 10 task)

--- day9.py
+++ day9.py
@@ -1,15 +1,18 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from matplotlib import colors
 
 st.header("Line graph of random numbers")
 
 df = pd.DataFrame({'y': np.random.rand(100)})
 
-st.write("Here's the dataframe I'll be charting", df.head())
+st.write("Here's the head of the dataframe I'll be charting", df.head())
 
-st.write("And here's the chart!")
+color_choices = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+color = st.selectbox("Please select a color for the line", options = color_choices)
+st.write("you picked", color, " aka ", colors.to_rgba(color))
 
-st.line_chart(df, y="y")
+st.line_chart(df, y="y", color=colors.to_rgba(color))
 
 st.write("Github repo [here](https://github.com/arilamstein/day9)")
